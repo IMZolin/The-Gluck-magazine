9665f769e700222de0e7bafba3480cd246549333
IMZolin <Zolin5269@gmail.com>
2022-09-22 00:09:02 +0300

    Init commit

---
+++ src/main/java/com/example/thegluck/TheGluckApplication.java
@@ -0,0 +1,13 @@
+package com.example.thegluck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGluckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheGluckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thegluck/TheGluckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thegluck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGluckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
