eb06eba0b697ac673a5d1e53cb26dddb7509a896
IMZolin <Zolin5269@gmail.com>
2022-09-30 22:37:37 +0300

    Add User and Article classes

--- src/main/java/com/example/thegluck/domain/Article.java
+++ src/main/java/com/example/thegluck/domain/Article.java
@@ -19,4 +19,28 @@ public class Article {
         this.text = text;
         this.tag = tag;
     }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getText() {
+        return text;
+    }
+    public String getTag() {
+        return tag;
+    }
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
---
+++ src/main/java/com/example/thegluck/domain/User.java
@@ -0,0 +1,77 @@
+package com.example.thegluck.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String fist_name;
+    private String last_name;
+    private String email;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+    private String password;
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return fist_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.fist_name = fist_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+}
