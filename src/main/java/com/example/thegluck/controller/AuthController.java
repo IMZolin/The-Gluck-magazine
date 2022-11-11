9a161ae61c2d0f01386280873f0edf6928d2fdfb
IMZolin <Zolin5269@gmail.com>
2022-11-11 15:48:53 +0300

    Modify login ad signup. Add encoding password. Add token.

--- src/main/java/com/example/thegluck/controller/AuthController.java
+++ src/main/java/com/example/thegluck/controller/AuthController.java
@@ -4,16 +4,19 @@ import com.example.thegluck.entity.UserEntity;
 import com.example.thegluck.exception.NotMatchPasswordException;
 import com.example.thegluck.exception.UserAlreadyExistException;
 import com.example.thegluck.exception.UserNotFoundException;
-import com.example.thegluck.service.LoginService;
+import com.example.thegluck.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/api")
-public class LoginController {
+public class AuthController {
     @Autowired
-    private LoginService loginService;
+    private AuthService authService;
     record SignupRequest(String username,
                          String first_name,
                          String last_name,
@@ -27,10 +30,13 @@ public class LoginController {
                           String last_name,
                           String email
     ){}
-    @PostMapping(value ="/auth/signup")
-    public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) throws UserAlreadyExistException, NotMatchPasswordException {
+    record LoginRequest(String email, String password){}
+    record LoginResponse(String token){}
+
+    @PostMapping(value ="/signup")
+    public ResponseEntity<?> signup(@RequestBody SignupRequest signupRequest) throws UserAlreadyExistException, NotMatchPasswordException {
         try {
-            var user = loginService.signup(
+            var user = authService.signup(
                     signupRequest.username(),
                     signupRequest.first_name(),
                     signupRequest.last_name(),
@@ -38,18 +44,27 @@ public class LoginController {
                     signupRequest.password(),
                     signupRequest.password_confirm()
             );
-            return ResponseEntity.ok("User was saved");
+            SignupResponse signupResponse = new SignupResponse(user.getId(),user.getUsername(), user.getFirst_name(), user.getLast_name(),user.getEmail());
+            return ResponseEntity.ok(signupResponse);
         } catch (UserAlreadyExistException | NotMatchPasswordException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("Error");
         }
     }
-    @PostMapping("/auth/login")
-    public ResponseEntity login(@RequestBody UserEntity user) {
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
         try {
-            String userName = loginService.login(user);
-            return ResponseEntity.ok(userName);
+            /*String userName = loginService.login(user);
+            return ResponseEntity.ok(userName);*/
+            var login = authService.login(loginRequest.email(), loginRequest.password());
+            Cookie cookie = new Cookie("refresh_token", login.getRefreshToken().getToken());
+            cookie.setMaxAge(3600);
+            cookie.setHttpOnly(true);
+            cookie.setPath("/api");
+            response.addCookie(cookie);
+            LoginResponse loginResponse  = new LoginResponse(login.getAccessToken().getToken());
+            return ResponseEntity.ok(loginResponse);
         } catch (UserNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         } catch (Exception e) {
--- src/main/java/com/example/thegluck/service/AuthService.java
+++ src/main/java/com/example/thegluck/service/AuthService.java
@@ -1,21 +1,30 @@
 package com.example.thegluck.service;
 
-import com.example.thegluck.entity.RoleEntity;
 import com.example.thegluck.exception.NotMatchPasswordException;
 import com.example.thegluck.entity.UserEntity;
 import com.example.thegluck.exception.UserAlreadyExistException;
 import com.example.thegluck.exception.UserNotFoundException;
-import com.example.thegluck.model.ERole;
 import com.example.thegluck.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
 @Service
-public class LoginService {
+public class AuthService {
     @Autowired
     private UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private final String accessTokenSecret;
+    private final String refreshTokenSecret;
+    public AuthService(PasswordEncoder passwordEncoder, @Value("${application.security.access-token-secret}") String accessTokenSecret, @Value("${application.security.refresh-token-secret}") String refreshTokenSecret) {
+        this.passwordEncoder = passwordEncoder;
+        this.accessTokenSecret = accessTokenSecret;
+        this.refreshTokenSecret = refreshTokenSecret;
+    }
+
     public UserEntity signup(String username, String first_name, String last_name, String email, String password, String password_confirm) throws NotMatchPasswordException, UserAlreadyExistException {
         if (userRepo.findByEmail(email) != null)
             throw new UserAlreadyExistException("User with this email have already registered");
@@ -25,17 +34,19 @@ public class LoginService {
             throw new NotMatchPasswordException("Passwords don't match");
 
         return userRepo.save(
-                UserEntity.of(username,first_name,last_name, email,password)
+                UserEntity.of(username,first_name,last_name, email,passwordEncoder.encode(password))
         );
     }
-    public String login(UserEntity user) throws UserNotFoundException {
-        UserEntity checkingUser = (UserEntity) userRepo.findByEmail(user.getEmail());
-        if (checkingUser == null) {
+    public Login login(String email, String password) throws UserNotFoundException {
+        UserEntity user = (UserEntity) userRepo.findByEmail(email);
+        if (user == null) {
             throw new UserNotFoundException("User with this email has not been registered yet");
         }
-        if (!checkingUser.getPassword().equals(user.getPassword())) {
+        //checkingUser.getPassword()
+        if (!passwordEncoder.matches(password,user.getPassword())) {
             throw new UserNotFoundException("User with this email had different password");
         }
-        return checkingUser.getUsername();
+        /*return checkingUser.getUsername();*/
+        return Login.of(user.getId(),accessTokenSecret, refreshTokenSecret);
     }
 }
